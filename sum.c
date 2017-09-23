97f241511718d83371b965a43284162c65954cc4
snekhadhanu <31586807+snekhadhanu@users.noreply.github.com>
2017-09-23 06:06:30 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,num,sum=0;
+printf("enter the value of n");
+scanf("%d",&n);
+fot(num=1;num<=n;num++)
+{
+sum=sum+num;
+}
+printf("sum of n natural numbers is %d",sum);
+getch();
+}

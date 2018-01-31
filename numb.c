bd1baa170a28d6d23e2bea744b7e3698a97c50d1
snekhadhanu <31586807+snekhadhanu@users.noreply.github.com>
2018-01-31 16:50:18 +0530

    Create numb.c

---
+++ numb.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  printf("enter the number");
+  scanf("%d",&n);
+  if(n==10||n==1||n==2||n==3||n==4||n==5||n==6||n==7||n==8||n==9)
+  {
+    printf("yes");
+  }
+  return 0;
+}

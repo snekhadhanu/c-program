018216a9528498a0f3a6ffe90249ae9074bdc9a9
snekhadhanu <31586807+snekhadhanu@users.noreply.github.com>
2018-02-02 16:27:47 +0530

    Create oddless.c

---
+++ oddless.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+void main()
+{
+  int a;
+  printf("enter the number");
+  scanf("%d",&a);
+  if(a%2==0)
+  {
+    printf("%d",a);
+  }
+  else
+  {
+    printf("%d",--a);
+  }
+}

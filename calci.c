97829435cae3a081d54b682a4c9f92865e9b7933
snekhadhanu <31586807+snekhadhanu@users.noreply.github.com>
2018-02-28 16:58:49 +0530

    Create calci.c

---
+++ calci.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+  int a,b,ch;
+  printf("enter the choice");
+  scanf("%d",&ch);
+  printf("enter the values of a and b");
+  scanf("%d%d",&a,&b);
+  switch(ch)
+{
+  case 1:
+  {
+  printf("%d",a*b);
+  }
+  case 2:
+  {
+    printf("%d",a%b);
+  }
+}
+}
